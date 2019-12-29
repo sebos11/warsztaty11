30120e5e2017cb0166c2fa079d1f25ccc98aa98f
sebos11 <sszast@gmail.com>
2019-12-29 14:15:54 +0100

    Zmiana zadania 1 użytkownik podaje zakres losowania liczb i limit prób w jakich zgadnie wylosowaną liczbę

--- src/pl/coderslab/warszataty11/zgaduj/gra.java
+++ src/pl/coderslab/warszataty11/zgaduj/gra.java
@@ -7,24 +7,39 @@ public class gra {
     public static void main(String[] args) {
         System.out.println("zadanie 1");
         Random losowa = new Random();
-        int wylosowanaLiczba = losowa.nextInt(100) + 1;
+        int zakres = 10;
+        int licznik = 0;
+        int limit = 1;
+        System.out.print("Ustal górną granicę przedziału liczbowego z którego mam wylosować liczbę <1 , X>  X=  ");
+        zakres = wczytajLiczbe();
+        System.out.print("Podaj w ilu próbach odgadniesz wylosowaną liczbę - Liczba prób = ");
+        limit = wczytajLiczbe();
 
-        System.out.println("wylosowałem liczbę zgadnij jaką ?");
-        Boolean win = false;
-        while   (!win) {
-            System.out.println("podaj liczbę");
+        int wylosowanaLiczba = losowa.nextInt(zakres) + 1;
+
+        System.out.println("wylosowałem liczbę zgadnij jaką ? MASZ  " + limit +"  prób");
+
+        boolean win = false;
+        while (!win) {
+            System.out.print("podaj liczbę = ");
             int liczba = wczytajLiczbe();
-
+            licznik++;
             if (liczba > wylosowanaLiczba) {
-                System.out.println("podana liczba jest za duża");
-            } else if (liczba < wylosowanaLiczba) {
-                System.out.println("wylosowana liczba jest za mała");
-            } else {
-                System.out.println("zgadłeś!!!!");
-                win = true;
+                System.out.println("podana liczba jest za duża - Twoja próba nr  " + licznik + "  z " + limit+"  prób ");
             }
-        }
+             if (liczba < wylosowanaLiczba) {
+                 System.out.println("podana liczba jest za mała - Twoja próba nr " + licznik + " z "+ limit +"  prób");
+             }
+              if (liczba == wylosowanaLiczba) {
+                      System.out.println("zgadłeś!!!!  w próbie nr " + licznik);
+                       win = true;
+                   }
+              if (licznik == limit && !win) {
+                  System.out.println("wyczerpałeś limit prób - KONIEC GRY");
+                  win = true;
+              }
 
+        }
     }
     public static int wczytajLiczbe(){
         Scanner wczytaj = new Scanner(System.in);
