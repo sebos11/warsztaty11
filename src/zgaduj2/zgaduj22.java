91a8c89b44be28ab2963e19f726f51e755151026
sebos11 <sszast@gmail.com>
2020-03-18 07:25:06 +0100

    Zadanie Zgaduj2 działa (możliwe poprawki)

---
+++ src/zgaduj2/zgaduj22.java
@@ -0,0 +1,63 @@
+package zgaduj2;
+
+
+import java.util.Scanner;
+
+public class zgaduj22 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int guess;
+        int min = 0, max = 1000;
+        boolean win = false;
+        int count = 1;
+
+        System.out.println("Pomyśl liczbę od 0 d0 1000,");
+        System.out.println("a ja ją zgadnę w max. 10 próbach.");
+
+        while (!win) {
+
+            guess = (int) (((max - min) / 2) + min);
+            System.out.println("Próba nr " + count+ " Zgaduję: " + guess);
+            System.out.println("Wciśnij: 1 - jeśli za dużo");
+            System.out.println("Wciśnij: 2 - jeśli za mało");
+            System.out.println("Wciśnij: 3 - jeśli zgadłem");
+
+
+            int userAnswer = scanner.nextInt();
+
+
+            switch (userAnswer) {
+                case 1: {
+                    max = guess;
+                    count++;
+
+
+                    break;
+                }
+                case 2: {
+                    min = guess;
+                    count++;
+                    break;
+                }
+                case 3:
+                    System.out.println("WYGRAŁEM!!!");
+                    win = true;
+                    break;
+                default: {
+                    System.out.println("Wybierz właściwą liczbę");
+
+                }
+
+            }
+
+            if (count > 10){
+                System.out.println("OSZUKAŁEŚ");
+                win = true;
+            }
+            System.out.println("MIN = " + min + " MAX = " + max);
+        }
+    }
+
+
+}
