397e30fec443f732972f3d79fac6d3d1449a56ed
sebos11 <sszast@gmail.com>
2020-04-09 23:09:57 +0200

    wyszukiwarka prawie koniec

--- src/pl/coderslab/warszataty11/wyszukiwarka/tester.java
+++ src/pl/coderslab/warszataty11/wyszukiwarka/tester.java
@@ -15,11 +15,13 @@ import java.util.List;
 
 public class tester {
     public static void main(String[] args) {
+        String[] omittedWords = {"już","tylko","przez","USA","JEGO","TWÓJ","tak","jak","pod","czy","raz","oraz","oto","nie","ponieważ","lecz","może","być","sam","się","lub","dla","ws."};
 
         Path path1 = Paths.get("popular_words.txt");
         Path path2 = Paths.get("filtered_popular_words.txt");
         List<String> lines = new ArrayList<>();
-        String temp;
+        List<String> outList = new ArrayList<>();
+
 
 
         Connection connect = Jsoup.connect("http://www.onet.pl/");
@@ -28,7 +30,7 @@ public class tester {
             Elements links = document.select("span.title");
             for (Element elem : links) {
                 lines.add(elem.text());
-                //System.out.println(elem.text());
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -45,15 +47,61 @@ public class tester {
             for (String line : Files.readAllLines(path1)) {
                 String[] parts = line.split(" ");
                 for (int i =0; i<parts.length;i++){
-                    System.out.println(parts[i]);
-                }
+                    String temp1 = parts[i];
+                   temp1 = ommitsCharInString(temp1);
+                    if (isPopular(temp1,omittedWords)) {
 
-               // System.out.println(line);
+                        outList.add(temp1);
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        try {
+            Files.write(path2, outList);
+        } catch (IOException ex) {
+            System.out.println("Nie można zapisać pliku.");
+        }
+
+
+
     }
+    public static boolean isPopular(String wordToTest, String[] omittedWords) {
+
+        if (wordToTest.length() < 3) {
+            return false;
+        } else {
+            for (int i = 0; i < omittedWords.length; i++) {
+
+                if (wordToTest.toLowerCase().equals(omittedWords[i].toLowerCase())){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+public static boolean isOmittedChar(char c,char[] omittedChar){
+        for (int i =0; i< omittedChar.length;i++){
+            if (c == omittedChar[i]) {
+
+                return true;
+            }
+        }
+        return false;
+}
+public static String ommitsCharInString(String s){
+    char[] omittedChar ={',','.','?',':',';','!','-','"','#'};
+        StringBuilder temp = new StringBuilder();
+        for (int i = 0; i<s.length();i++){
+            if (!isOmittedChar(s.charAt(i),omittedChar)) {
+                temp.append(s.charAt(i));
+            }
+        }
+    return temp.toString();
+}
 }
    /* Wywołaj pobieranie dla wybranych serwisów internetowych.
         Pomiń wszystkie elementy krótsze niż 3-znakowe.
